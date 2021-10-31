b50802488626ac2fa36417067de1533292196a06
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-10-31 14:28:56 -0700

    add input validations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-'use strict';
+const NUM_REGEX = /[^0-9\.]+/g;
+const PEOPLE_REGEX = /[^0-9]+/g;
+const LETTER_REGEX = /[a-zA-Z,/<>\?;':""[\]\\{}\|`~!@#\$%\^&\*()_=\+]+/g;
+const NEGATIVE_REGEX = /[\-]+/g;
+const ZERO_REGEX = /0\d+/g;
 
 const resetBtn = document.querySelector('.js-reset');
 const customInput = document.querySelector('.js-input--custom');
@@ -8,13 +12,15 @@ const numbers = document.querySelectorAll('.js-number');
 const errorTexts = document.querySelectorAll('.js-error-text');
 const percentageBtns = document.querySelectorAll('.js-percentage');
 
-const [billInput, peopleInput] = inputs;
+const [billInput, custom, peopleInput] = inputs;
 const [tipAmount, totalAmount] = numbers;
-const [billErrorText, peopleErrorText] = errorTexts;
+const [billErrorText, percentageErrorText, peopleErrorText] = errorTexts;
 
-const setActiceState = function () {
+const setActiveState = function () {
   items.forEach((item) => {
     item.addEventListener('click', (e) => {
+      resetBtn.removeAttribute('disabled');
+
       items.forEach((i) => i.classList.remove('calc__item--active'));
       e.target.classList.add('calc__item--active');
     });
@@ -23,50 +29,77 @@ const setActiceState = function () {
 
 const setErrorState = function (e, msg) {
   e.target.previousElementSibling.lastElementChild.textContent = msg;
-  e.target.classList.add('calc__error-input');
+  e.target.style.outline = '0.2rem solid hsl(10, 44%, 59%)';
   items.forEach((i) => i.classList.remove('calc__item--active'));
 };
 
 const setSuccessState = function (e) {
   e.target.previousElementSibling.lastElementChild.textContent = '';
-  e.target.classList.remove('calc__error-input');
+  e.target.style.outline = '0.2rem solid hsl(172, 67%, 45%)';
 };
 
-const validateInput = function (e) {
+const validateBillInput = function (e) {
+  let value = e.target.value;
+
+  resetBtn.removeAttribute('disabled');
+  // e.target.value = e.target.value.replace(/[a-zA-Z\\\_]*/g, '');
+
+  if (!value || value === '0') {
+    setErrorState(e, "Can't be zero");
+  } else if (NUM_REGEX.test(value) || LETTER_REGEX.test(value))
+    setErrorState(e, "Can't be special character");
+  else if (value < 0 || NEGATIVE_REGEX.test(value))
+    setErrorState(e, "Can't be negative");
+  else if (ZERO_REGEX.test(value)) setErrorState(e, "Can't start with zero");
+  else setSuccessState(e);
+};
+
+const validateCustomInput = function (e) {
   const value = e.target.value;
-  const regex = /[0-9/\.]+/g;
 
   resetBtn.removeAttribute('disabled');
 
-  if (!value || value === '' || value === '0')
-    setErrorState(e, "Can't be zero");
-  else if (!regex.test(value)) setErrorState(e, 'No letters allowed');
-  else if (value < 0) setErrorState(e, "Can't be negative");
+  if (NUM_REGEX.test(value) || LETTER_REGEX.test(value))
+    customInput.style.outline = '0.2rem solid hsl(10, 44%, 59%)';
+  else if (NUM_REGEX.test(value) || value < 0 || NEGATIVE_REGEX.test(value))
+    customInput.style.outline = '0.2rem solid hsl(10, 44%, 59%)';
+  else customInput.style.outline = '0.2rem solid hsl(172, 67%, 45%)';
+
+  if (value === '') customInput.style.outline = 0;
+};
+
+const validatePeopleInput = function (e) {
+  const value = e.target.value;
+
+  resetBtn.removeAttribute('disabled');
+
+  if (!value || value === '0') setErrorState(e, "Can't be zero");
+  else if (PEOPLE_REGEX.test(value) || LETTER_REGEX.test(value))
+    setErrorState(e, 'Only numbers allowed');
+  else if (value < 0 || NEGATIVE_REGEX.test(value))
+    setErrorState(e, "Can't be negative");
+  else if (ZERO_REGEX.test(value)) setErrorState(e, "Can't start with zero");
   else setSuccessState(e);
 };
 
 const resetAll = function () {
   customInput.value = '';
+  customInput.style.outline = '0';
   inputs.forEach((el) => {
     el.value = '';
-    el.classList.remove('calc__error-input');
+    el.style.outline = '0';
   });
   errorTexts.forEach((el) => (el.textContent = ''));
   items.forEach((i) => i.classList.remove('calc__item--active'));
+
+  if ([billInput, peopleInput, customInput].every((el) => el.value === ''))
+    resetBtn.setAttribute('disabled', true);
 };
 
 resetBtn.addEventListener('click', resetAll);
-inputs.forEach((el) => el.addEventListener('keyup', validateInput));
-
-customInput.addEventListener('keyup', (e) => {
-  const value = e.target.value;
-  if (!value || value === '' || value === '0' || !/[0-9/\.]+/.test(value)) {
-    e.target.classList.add('calc__error-input');
-  } else {
-    e.target.classList.remove('calc__error-input');
-    resetBtn.removeAttribute('disabled');
-  }
-});
+billInput.addEventListener('keyup', validateBillInput);
+customInput.addEventListener('keyup', validateCustomInput);
+peopleInput.addEventListener('keyup', validatePeopleInput);
 
 window.onload = () =>
   [...inputs].map((el) => {
@@ -75,5 +108,5 @@ window.onload = () =>
   });
 
 (() => {
-  setActiceState();
+  setActiveState();
 })();
