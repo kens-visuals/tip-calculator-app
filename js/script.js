ddf297ce58035b57c8c74421f8b3e5c134f174bf
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-11-01 18:18:41 -0700

    polish JS

--- js/script.js
+++ js/script.js
@@ -1,43 +1,58 @@
-const ZERO_REGEX = /0\d+/g;
-const NUM_REGEX = /[^0-9\.]+/g;
-const PEOPLE_REGEX = /[^0-9]+/g;
-const NEGATIVE_REGEX = /[\-]+/g;
-const LETTER_REGEX = /[a-zA-Z,/<>\?;':""[\]\\{}\|`~!@#\$%\^&\*()_=\+]+/g;
+const regExes = {
+  zero: /0\d+/g,
+  num: /[^0-9\.]+/g,
+  people: /[^0-9]+/g,
+  letter: /[a-zA-Z,/<>\?;':""[\]\\{}\|`~!@#\$%\^&\*()_=\+]+/g,
+};
+
+const colors = {
+  primary: 'hsl(183, 100%, 15%)',
+  error: 'hsl(10, 44%, 59%)',
+  success: 'hsl(172, 67%, 45%)',
+};
 
 const resetBtn = document.querySelector('.js-reset');
-const customInput = document.querySelector('.js-input--custom');
 const items = document.querySelectorAll('.js-item');
 const inputs = document.querySelectorAll('.js-input');
 const numbers = document.querySelectorAll('.js-number');
 const errorTexts = document.querySelectorAll('.js-error-text');
 const percentageBtns = document.querySelectorAll('.js-percentage');
 
-const [billInput, custom, peopleInput] = inputs;
+const [billInput, customInput, peopleInput] = inputs;
 const [tipAmount, totalAmount] = numbers;
-const [billErrorText, percentageErrorText, peopleErrorText] = errorTexts;
 
-const testRegexp = (regex, val) => new RegExp(regex).test(val);
+const testRegExp = (regex, val) => new RegExp(regex).test(val);
+
+const errorStyle = (input) => {
+  input.style.outline = `0.2rem solid ${colors.error}`;
+  input.style.color = `${colors.error}`;
+  input.style.caretColor = `${colors.error}`;
+};
+const succesStyle = (input) => {
+  input.style.outline = `0.2rem solid ${colors.success}`;
+  input.style.color = `${colors.primary}`;
+  input.style.caretColor = `${colors.success}`;
+};
 
 const setActiveState = function () {
   items.forEach((item) => {
     item.addEventListener('click', (e) => {
       resetBtn.removeAttribute('disabled');
 
-      items.forEach((i) => i.classList.remove('calc__item--active'));
+      items.forEach((item) => item.classList.remove('calc__item--active'));
       e.target.classList.add('calc__item--active');
     });
   });
 };
 
 const setErrorState = function (e, msg) {
+  errorStyle(e.target);
   e.target.previousElementSibling.lastElementChild.textContent = msg;
-  e.target.style.outline = '0.2rem solid hsl(10, 44%, 59%)';
   items.forEach((i) => i.classList.remove('calc__item--active'));
 };
-
 const setSuccessState = function (e) {
+  succesStyle(e.target);
   e.target.previousElementSibling.lastElementChild.textContent = '';
-  e.target.style.outline = '0.2rem solid hsl(172, 67%, 45%)';
 };
 
 const validateInput = function (e, ...regex) {
@@ -46,78 +61,53 @@ const validateInput = function (e, ...regex) {
   resetBtn.removeAttribute('disabled');
 
   if (!value || value === '0') setErrorState(e, "Can't be zero");
-  else if (testRegexp(regex[0], value) || testRegexp(regex[1], value))
+  else if (testRegExp(regex[0], value) || testRegExp(regex[1], value))
     setErrorState(e, 'Positive numbers only');
-  else if (testRegexp(regex[2], value))
+  else if (testRegExp(regex[2], value))
     setErrorState(e, "Can't start with zero");
+  else if (value.split(/[\.]/).length > 2)
+    setErrorState(e, "Can't have two dots");
   else setSuccessState(e);
 };
-
-// const validateBillInput = function (e) {
-//   let value = e.target.value;
-
-//   resetBtn.removeAttribute('disabled');
-//   // e.target.value = e.target.value.replace(/[a-zA-Z\\\_]*/g, '');
-
-//   if (!value || value === '0') {
-//     setErrorState(e, "Can't be zero");
-//   } else if (NUM_REGEX.test(value) || LETTER_REGEX.test(value))
-//     setErrorState(e, "Can't be special character");
-//   else if (value < 0 || NEGATIVE_REGEX.test(value))
-//     setErrorState(e, "Can't be negative");
-//   else if (ZERO_REGEX.test(value)) setErrorState(e, "Can't start with zero");
-//   else setSuccessState(e);
-// };
 
 const validateCustomInput = function (e) {
   const value = e.target.value;
 
   resetBtn.removeAttribute('disabled');
 
-  if (NUM_REGEX.test(value) || LETTER_REGEX.test(value))
-    customInput.style.outline = '0.2rem solid hsl(10, 44%, 59%)';
-  else if (NUM_REGEX.test(value) || value < 0 || NEGATIVE_REGEX.test(value))
-    customInput.style.outline = '0.2rem solid hsl(10, 44%, 59%)';
-  else customInput.style.outline = '0.2rem solid hsl(172, 67%, 45%)';
+  if (testRegExp(regExes.num, value) || testRegExp(regExes.letter, value))
+    errorStyle(e.target);
+  else if (value < 0) errorStyle(e.target);
+  else if (value.split(/[\.]/).length > 2) errorStyle(e.target);
+  else succesStyle(e.target);
 
-  if (value === '') customInput.style.outline = 0;
+  value === '' && (customInput.style.outline = '0');
+
+  items.forEach((item) => item.classList.remove('calc__item--active'));
 };
 
-// const validatePeopleInput = function (e) {
-//   const value = e.target.value;
-
-//   resetBtn.removeAttribute('disabled');
-
-//   if (!value || value === '0') setErrorState(e, "Can't be zero");
-//   else if (PEOPLE_REGEX.test(value) || LETTER_REGEX.test(value))
-//     setErrorState(e, 'Only numbers allowed');
-//   else if (value < 0 || NEGATIVE_REGEX.test(value))
-//     setErrorState(e, "Can't be negative");
-//   else if (ZERO_REGEX.test(value)) setErrorState(e, "Can't start with zero");
-//   else setSuccessState(e);
-// };
-
 const resetAll = function () {
-  customInput.value = '';
-  customInput.style.outline = '0';
   inputs.forEach((el) => {
     el.value = '';
-    el.style.outline = '0';
+    el.style.outline = 0;
+    el.style.color = `${colors.primary}`;
+    el.style.caretColor = `${colors.success}`;
   });
-  errorTexts.forEach((el) => (el.textContent = ''));
-  items.forEach((i) => i.classList.remove('calc__item--active'));
 
-  if ([billInput, peopleInput, customInput].every((el) => el.value === ''))
+  errorTexts.forEach((el) => (el.textContent = ''));
+  items.forEach((item) => item.classList.remove('calc__item--active'));
+
+  [billInput, peopleInput, customInput].every((el) => el.value === '') &&
     resetBtn.setAttribute('disabled', true);
 };
 
 resetBtn.addEventListener('click', resetAll);
 customInput.addEventListener('keyup', validateCustomInput);
 billInput.addEventListener('keyup', (e) =>
-  validateInput(e, NUM_REGEX, LETTER_REGEX, ZERO_REGEX)
+  validateInput(e, regExes.num, regExes.letter, regExes.zero)
 );
 peopleInput.addEventListener('keyup', (e) =>
-  validateInput(e, PEOPLE_REGEX, LETTER_REGEX, ZERO_REGEX)
+  validateInput(e, regExes.people, regExes.letter, regExes.zero)
 );
 
 window.onload = () =>
